1128987cce4ef30ce4e9be0d0c0f919e112258a0
Opilane TTHK <opilane@tthk.ee>
2021-02-23 12:21:41 +0200

    HarryPotter

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card(props) {
+    return <div>
+        <h1>{props.name}</h1>
+        <img src={props.img} alt="" />
+        <p>{props.email}</p>
+    </div>
+}
+
+export default Card;
